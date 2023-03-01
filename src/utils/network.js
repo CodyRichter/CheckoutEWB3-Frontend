be84e9098601100691cbb2a94f084fdd8a7a2eec
Cody Richter <crichter@umass.edu>
2023-03-01 17:05:36 -0500

    Update Network URL

--- src/utils/network.js
+++ src/utils/network.js
@@ -1,6 +1,6 @@
 import { get, has, isEmpty } from "lodash";
 
-const API_ROOT = "http://localhost:4250/";
+const API_ROOT = "https://checkout-ewb.herokuapp.com/";
 
 class Network {
   static async login(username, password) {
